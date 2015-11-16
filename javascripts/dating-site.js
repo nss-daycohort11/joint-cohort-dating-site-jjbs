d0cf081fdfe85fd8e26051900db898c6a3ecfe17
Jeremy <jeremygrondahl@gmail.com>
2015-11-16 14:18:48 -0600

    Implemented authentication via facebook.

--- javascripts/dating-site.js
+++ javascripts/dating-site.js
@@ -5,16 +5,35 @@ require.config({
     'lodash': '../lib/bower_components/lodash/lodash.min',
     'hbs': '../lib/bower_components/require-handlebars-plugin/hbs',
     'q': '../lib/bower_components/q/q',
+    'firebase': '../lib/bower_components/firebase/firebase',
     'bootstrap': '../lib/bower_components/bootstrap/dist/js/bootstrap.min'
   },
   shim: {
-    'bootstrap': ['jquery']
+    'bootstrap': ['jquery'],
+    'firebase': {
+      exports: 'Firebase'
+    }
   }
 });
 
 require(
-  ["dependencies"], 
-  function(_$_) {
+  ["jquery","lodash", "hbs","q", "bootstrap", "firebase", "auth"], 
+  function($,_, Handlebars, Q, bootstrap, firebase, auth) {
+  var ref = new Firebase("https://superdate.firebaseio.com");
+  var authData = ref.getAuth();
+  console.log("authData", authData);
+
+  $("#login").on("click", function() {
+    console.log("authData", authData);
+    ref.authWithOAuthPopup("facebook", function(error, authData) {
+      if (error) {
+        console.log("Login Failed!", error);
+      } 
+      else {
+        console.log("Authenticated successfully with payload:", authData);
+      }
+    });
+  });
 
     /*
       You can choose to use the REST methods to interact with
@@ -24,6 +43,8 @@ require(
       If you choose the former, I created two boilerplate modules
       named `potential-mates.js`, and `add-favorite.js`.
      */
+
+
     
   }
 );
