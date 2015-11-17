487587df336f492af430685f8d0823409f315e7c
bbleds <bledsoedesigns@gmail.com>
2015-11-16 22:06:14 -0600

    added filter functionality, and profile styling

--- javascripts/dating-site.js
+++ javascripts/dating-site.js
@@ -14,8 +14,100 @@ require.config({
 });
 
 require(
-  ["dependencies"], 
-  function(_$_) {
+  ["jquery", "lodash", "filter_mates", "q", "attachProfileClickEvent", "varsPassed"], 
+  function($,_,filter_mates, Q, proClick, varsPassed) {
+
+    //Show signed-in profile OR dashboard
+      $("#pro_nav").click(function(){
+        $("#main_output").css("display", "none");
+
+         require(["hbs!../templates/signed_indiv_profile"], function(mateTemplate){
+                $("#signed_in_user_profile_panel").html(mateTemplate(varsPassed.getCurrentUser()));
+              });
+
+        $("#signed_in_user_profile_panel").fadeIn("slow");
+      });
+
+      $("#home_nav").click(function(){
+        $("#signed_in_user_profile_panel").css("display", "none");
+        $("#main_output").fadeIn("slow");
+      });
+
+
+
+
+
+    //Filter functionality  
+        //Filter button is disabled until a value is selected on radio buttons
+         $('input:radio[name=power-type]').change(function(){
+            $(".filter-btn").attr("disabled", false);
+         });
+
+
+        //Filter mates on filter btn click
+        $(".filter-btn").click(function(){
+
+          var filteredUsers = {}
+          
+          console.log($('input:radio[name=power-type]:checked').val());
+
+          //ajax with a promise
+          filter_mates()
+          .then(function(returnedData){
+
+            //log data returned
+            console.log("data returned", returnedData);
+
+            //filter by value of radio button
+
+              //what to filter by
+              var filterBy = $('input:radio[name=power-type]:checked').val();
+
+              //set to lower case
+              var loweredFilter = filterBy.toLowerCase();
+
+              for(var key in returnedData){
+                if(returnedData[key].superPower.toLowerCase() === loweredFilter){
+
+                  //set each filtered user as a key on filteredUsers object
+                  filteredUsers[key] = returnedData[key];
+                }
+              }
+
+              console.log("filteredUsers", filteredUsers);
+
+              //set Filtered Object as accessible variables
+              varsPassed.setFiltered(filteredUsers);
+
+            //populate right side of main.html with filtered results (pass to hbs template)
+            require(["hbs!../templates/filter_mates"], function(mateTemplate){
+                $("#user-output").html(mateTemplate(filteredUsers));
+              });
+
+
+          }).done(function(){ 
+              proClick.attachClick();
+          });
+
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //on some profile click from filter, pop up other-profile.html panel page
+      //populate from firebase
+
+    //on your profile click on top of page, pop up your-profile.html panel page
+      //populate from firebase
 
 
 
