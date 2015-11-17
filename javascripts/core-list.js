5be7467d9a47f564dd1db99a1b4b3c3567b18d78
Sally Fisher <sqfisher@gmail.com>
2015-11-16 20:02:21 -0600

    Implemented core-list to load the rest of the app after authentication. Added placeholder for event-handlers

---
+++ javascripts/core-list.js
@@ -0,0 +1,30 @@
+define(function(require){
+  var _ = require("lodash");
+  var fb = require("firebase");
+  var auth = require("auth");
+  var eventHand = require("event-handlers");
+  var popdom = require("populate-dom"); // This is where the firebase data will be sent
+
+  var ref = new Firebase("https://superdate.firebaseio.com");
+  ref.child("users").once("value", function(snapshot){
+    var users = snapshot.val();
+    console.log("users", users);
+    //popdom.displayData(users);
+  });
+
+  ref.child("users").orderByChild('uid').equalTo(auth.getUid()).once("value", function(snapshot){
+    var myprofile = snapshot.val();
+    console.log("myProfile", myprofile);
+    //popdom.displayData(users);
+  });
+
+  ref.child("my-likes").orderByKey().equalTo(auth.getUid()).on("child_added", function(snapshot, prevChildKey){
+    var myLikes = snapshot.val();
+    console.log("myLikes", myLikes);
+  });
+
+  ref.child("my-admirers").orderByKey().equalTo(auth.getUid()).on("child_added", function(snapshot, prevChildKey){
+    var likedBy = snapshot.val();
+    console.log("likedBy", likedBy);
+  });
+});
