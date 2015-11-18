b47e0cca0258cd726bf760e55c671200cafabde1
bbleds <bledsoedesigns@gmail.com>
2015-11-18 14:57:47 -0600

    added comments and cleaned up uneeded code

--- javascripts/dating-site.js
+++ javascripts/dating-site.js
@@ -18,34 +18,50 @@ require.config({
 
 require(["dependencies", "firebase", "oauth", "auth", "check-user-status", "account", "templates", "logged-in-functionality"], 
   function(dependencies, firebase, oauth, auth, status, account, templates, loggedInFunctionality) {
-    
-    if(auth.getAuthData() === null) {
+
+/*    
+--> Directions: (section 2) (section 1 located in index.html)
+    2) Set up require config at top of the page, and add all shims (mainly bootstrap -> jquery) 
+    3) Pass in all javascript modules that you are working with above, in the require list  --> e.g. require(["yourModuleListHere", "another-module"])
+    4) after step 3, set up your (anonymous) function:
+          - This function has parameter(s)
+             -> e.g. firebase, loggedInFunctionality, etc. 
+          - These parameters reference the javascript modules that were set up in step 3
+          - The function will eventually hold/reference all app logic
+    5)  Set up an html file/template (such as index), that you can begin testing on
+          - the html handlebars templates are located in 'templates.js'
+    6) Set up authentication 
+*/
+  
+    // load login handlebars template inside body element
      $("body").html(templates.login());
-    }
 
+    // run function on login btn click
     $(document).on("click", "#login", function(){
-      oauth.load().then(function(authData) {
-        if (authData === null) {
-          // Load login template
-          // $("body").html(templates.login());
-        } else {
-          require(["core-list"], function(corelist) {
-            auth.setAuthData(authData);
-            $("body").html(templates.main());
-            loggedInFunctionality(auth.getAuthData());
+
+      //run load method from oauth.js
+      oauth.load()
+
+      .then(function(authData) {
+
+             // load core-list.js and run function after it's loaded
+              require(["core-list"], function(corelist) {
+
+              // run setAuthData method from auth.js, and pass in authData
+              // this stores authData so that we can access it in other modules
+              auth.setAuthData(authData);
+
+              // load main handlebars template inside body element
+              $("body").html(templates.main());
+
+              //pass authdata variable into the logged-in-functionality.js module
+              //then run logged in functionality module
+              loggedInFunctionality(auth.getAuthData());
+
+            });
           });
-        }
+
         
-      }).fail();
-
-    });
-
-
-
-
-
-
-  var ref = new Firebase("https://superdate.firebaseio.com");
-  var authData = ref.getAuth();
+      });
 
 });
--- javascripts/oauth.js
+++ javascripts/oauth.js
@@ -1,28 +1,43 @@
 define(["jquery", "firebase", "q", "auth", "templates", "logged-in-functionality"],
 	function($, fb, Q, auth, templates, loggedInFunctionality) {
-  var q = require("q");
-  var ref = new Firebase("https://superdate.firebaseio.com");
-  var authData = ref.getAuth();
+
+		  // Reference entre app (all objects) in firebase 
+		  var ref = new Firebase("https://superdate.firebaseio.com");
+
+		  //run getAuth method on firebase object above (ref and store result in authData variable)
+		  	//--> this will: Synchronously retrieves the current authentication state of the client
+		  var authData = ref.getAuth();
 
   return {
   	load: function() {
-  		var deferred = q.defer();
 
+  		//begin promise
+  		var deferred = Q.defer();
+
+
+  		// run authWithOAuthPopup on firebase reference --> this takes: authWithOAuthPopup(provider, onComplete, [options])
 	      ref.authWithOAuthPopup("facebook", function(error, authData) {
+
+	      	//if there is an error with the authentication
 	        if (error) {
+
+	        	// reject promise/ deferred object, and pass error details back out of the promise
 	        	deferred.reject(error);
-	          console.log("Login Failed!", error);
+
+	        	//log the error
+	          	console.log("Login Failed!", error);
+
+	        //if there is no error with login
 	        } else {
+
+	        	// Resolve the promise and return data from firebase (authData variable we created above)
 	        	deferred.resolve(authData);
-	          require(["core-list"], function(corelist) {
-	            auth.setAuthData(authData);
-	            $("body").html(templates.main());
-	            loggedInFunctionality(auth.getAuthData());
-	          });
 	        }
+	        
 	      });  		
 
-  		return deferred.promise;
+			//return state of promise
+  			return deferred.promise;
   	}
   };
 
