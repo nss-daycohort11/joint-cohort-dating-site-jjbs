904e0f745d99b0e29fdb418724c300dbbaf18b77
Sally Fisher <sqfisher@gmail.com>
2015-11-17 21:48:49 -0600

    Checked user input for valid data and alerted user if field was left empty. If valid, posts profile to firebase and returns to home display.

--- javascripts/edit-profile.js
+++ javascripts/edit-profile.js
@@ -1,36 +1,63 @@
 define(function(require){
   var $ = require("jquery");
 
+
   $(document).on("click", "#profile_submit", function(){
     var ref = new Firebase("https://superdate.firebaseio.com/users");
     var authData = ref.getAuth();
     var profileObj = buildNewProfile(authData);
-    ref.child(authData.uid).set(profileObj);
+    if(profileObj){
+      ref.child(authData.uid).set(profileObj);
+      $("#signed_in_user_profile_panel").css("display", "none");
+      $("#edit_panel").css("display", "none");
+      $("#main_output").fadeIn("slow");
+    } else {
+      $(".alert-danger").show();
+    }
   });
 
   var buildNewProfile = function(data) {
-    var uid = data.auth.uid;
-    var facebook = data.facebook.cachedUserProfile;
-    var nameReal = facebook.name;
-    var gender = facebook.gender;
-    var image = facebook.picture.data.url;
-    var nameSuper = document.getElementById("supername").value;
-    var species = document.getElementById("species").value;
-    var location = document.getElementById("location").value;
-    var nemesis = document.getElementById("nemesis").value;
-    var superPower = document.editProfile.power.value;
-    var profileObj = {
-      "uid": uid,
-      "nameSuper": nameSuper,
-      "nameReal": nameReal,
-      "gender": gender,
-      "image": image,
-      "species": species,
-      "location": location,
-      "nemesis": nemesis,
-      "superPower": superPower
-    };
-    return profileObj;
+    var input = collectInputs();
+    var valid = validateInputs(input);
+    if (valid){
+      var uid = data.auth.uid;
+      var facebook = data.facebook.cachedUserProfile;
+      var nameReal = facebook.name;
+      var gender = facebook.gender;
+      var image = facebook.picture.data.url;
+      var profileObj = {
+        "uid": uid,
+        "nameSuper": input.nameSuper,
+        "nameReal": nameReal,
+        "gender": gender,
+        "image": image,
+        "species": input.species,
+        "location": input.location,
+        "nemesis": input.nemesis,
+        "superPower": input.superPower
+      };
+      return profileObj;
+    }
+  };
+
+  var collectInputs = function() {
+    var input = {};
+    input.nameSuper = document.getElementById("supername").value;
+    input.species = document.getElementById("species").value;
+    input.location = document.getElementById("location").value;
+    input.nemesis = document.getElementById("nemesis").value;
+    input.superPower = document.editProfile.power.value;
+    return input;
+  };
+
+  var validateInputs = function(input) {
+    var validInput = true;
+    for(var item in input){
+      if (!input[item]) {
+        validInput = false;
+      }
+    }
+    return validInput;
   };
 
 });
